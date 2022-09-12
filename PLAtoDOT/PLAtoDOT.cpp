92061944016f7df88175c2d97434609a70c8716b
sean920310 <sean920310@gmail.com>
2022-09-12 16:18:31 +0800

    tree init

--- PLAtoDOT/PLAtoDOT.cpp
+++ PLAtoDOT/PLAtoDOT.cpp
@@ -3,21 +3,31 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-typedef struct
+typedef struct Node
 {
 	string name;
-	int thenNode;
-	int elseNode;
-} Node;
+	Node* thenNode;
+	Node* elseNode;
+	Node* parentNode;
+
+	Node(string name,Node* thenNode=nullptr, Node* elseNode=nullptr, Node* parentNode=nullptr)
+	{
+		this->name = name;
+		this->thenNode = thenNode;
+		this->elseNode = elseNode;
+		this->parentNode = parentNode;
+	}
+}Node;
 
 int main()
 {
     fstream input("input.pla");
-	vector<Node> obdd,SoP;
-	int inputNum,outputNum, productNum;
+	vector<Node*> obdd;
+	int inputNum=0,outputNum=0, productNum=0;
 	while (!input.eof())
 	{
 		string buf,op;
@@ -35,17 +45,29 @@ int main()
 		}
 		else if (op == ".lib")
 		{
-			obdd.push_back({ "0" });
+			//init tree
+			Node* newNode = new Node("0");
+			obdd.push_back(newNode);
+
+			vector<string> nodeName;
 			for (int i = 1; i <= inputNum; i++)
 			{
-				Node node;
-				ss >> node.name;
+				ss >> buf;
 				for (int j = 0; j < i; j++)
 				{
-					obdd.push_back(node);
+					nodeName.push_back(buf);
 				}
 			}
-			obdd.push_back({ "1" });
+			queue<Node*> q;
+			Node* current = nullptr;
+			for (auto name :nodeName)
+			{
+				if (current == nullptr)
+				{
+
+				}
+			}
+			obdd.push_back(newNode);
 		}
 		else if (op == ".ob")
 		{
@@ -60,7 +82,7 @@ int main()
 			for (int i = 0; i < productNum; i++)
 			{
 				input >> buf;
-					
+				
 			}
 		}
 		else if (op == ".e")
